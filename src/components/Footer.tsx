18b3b68b92bdeb361c3889c5adf41e80b3a072b7
Ibrahim Abuobaida <ibobai@Ibrahims-Mac-mini.local>
2025-03-15 13:38:37 +0100

    Modify photo name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<FooterProps> = ({ language, t }) => {
           <div className="col-span-1 md:col-span-2">
             <div className="flex items-center gap-2 mb-4">
               <img
-                src="/public/logocircle.png"
+                src="/logoCircle.png"
                 alt="Company Logo"
                 className="h-8 w-8"
               />
